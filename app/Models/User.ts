bdda4ee99e4ce7570ee5d9c4a0dde2360d57193f
wakjoko <wakjoko@gmail.com>
2023-02-28 11:32:04 +0800

    add user model

---
+++ app/Models/User.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { column, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string | null
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
