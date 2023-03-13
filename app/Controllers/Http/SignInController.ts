496e0be7980f63d2b0b20025d8ad53de495d74eb
wakjoko <wakjoko@gmail.com>
2023-03-13 10:16:11 +0800

    refactoring and fine tuning

--- app/Controllers/Http/SignInController.ts
+++ app/Controllers/Http/SignInController.ts
@@ -1,11 +1,35 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import SignInValidator from 'App/Validators/SignInValidator';
+import SendVerifyEmailCase from 'App/Cases/SendVerifyEmailCase';
+import Event from '@ioc:Adonis/Core/Event'
 
 export default class SignInController {
   public async show({ view }: HttpContextContract) {
-    return view.render('signIn');
+    return view.render('guests/signIn')
   }
 
-  public async submit({ response }: HttpContextContract) {
-    return response.redirect().toRoute('dashboard')
+  public async submit({ request, response, auth, session }: HttpContextContract) {
+    const data = await request.validate(SignInValidator)
+    
+    try {
+      await auth.attempt(data.email, data.password, !!data.remember)
+    } catch (error) {
+      session.flash('errors.attempt', 'Email or password is incorrect')
+      return response.redirect().back()
+    }
+
+    if (!auth.user!.emailVerifiedAt) {
+      new SendVerifyEmailCase(auth.user!.email)
+      return response.redirect().toRoute('signup.verify')
+    }
+
+    if (auth.isLoggedIn && auth.user) {
+      /**
+       * Update last active timestamp
+       */
+      Event.emit('user:active', auth.user)
+    }
+
+    return response.redirect().toRoute('dashboard.show')
   }
 }
---
+++ resources/js/remove-hash-from-url.js
@@ -0,0 +1,17 @@
+var Utils = function() {
+    return {
+        removeHashFromUrl: function() {
+            var uri = window.location.toString();
+            var symbol = '#';
+
+            if (uri.indexOf(symbol) > 0) {
+                var cleaned = uri.substring(0, uri.indexOf(symbol));
+                window.history.replaceState({}, document.title, cleaned);
+            }
+        }
+    };
+}();
+
+$(document).ready(function() {
+    Utils.removeHashFromUrl();
+});
