10018fde58b420a3423442a3b58368e36d793bc7
wakjoko <wakjoko@gmail.com>
2023-03-13 10:16:11 +0800

    refactoring and fine tuning

--- app/Controllers/Http/SignUpController.ts
+++ app/Controllers/Http/SignUpController.ts
@@ -1,11 +1,17 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import User from 'App/Models/User'
+import SignUpValidator from 'App/Validators/SignUpValidator';
+import SendVerifyEmailCase from 'App/Cases/SendVerifyEmailCase';
 
 export default class SignUpController {
   public async show({ view }: HttpContextContract) {
-    return view.render('signUp')
+    return view.render('guests/signUp')
   }
 
-  public async submit({ response }: HttpContextContract) {
+  public async submit({ request, response }: HttpContextContract) {
+    const data = await request.validate(SignUpValidator)
+    const user = await User.create(data)
+    new SendVerifyEmailCase(user.email)
     return response.redirect().toRoute('signup.verify')
   }
 
